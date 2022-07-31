0400c21d849c4e980a04fbcb4a57a595d84cbbf7
k.omori <kentarou.omori0309@gmail.com>
2022-07-31 23:14:48 +0900

    initial commit.

---
+++ src/pages/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>aaaaa</div>
+  <div>{{ msg }}</div>
+  <span @click="increment">+</span>
+  <p>{{ count }}</p>
+  <span @click="decrement">-</span>
+
+  <div @click="linkTo('test')">テストへ遷移</div>
+
+  <div>
+    <button @click="setIsClick">おす</button>
+    <p v-if="isClick">クリックした！</p>
+    <p v-else>クリック解除した！</p>
+  </div>
+
+</template>
+
+<script setup>
+import { ref } from "vue"
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+
+const count = ref(0)
+const msg = 'Hello!'
+const isClick = ref(false)
+
+const increment = () => {
+  count.value++
+}
+const decrement = () => {
+  count.value--
+}
+const setIsClick = () => {
+  isClick.value = !isClick.value
+}
+const linkTo = (path) => {
+  router.push(`/${path}`)
+}
+</script>
---
+++ src/pages/Test.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>てスト</div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+</script>
